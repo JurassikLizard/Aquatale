a9bf0359483e41c9abdd7f7098910fbb471bda1d
UnsocialSamurott <Xboxboyx123@gmail.com>
2021-02-27 14:29:23 -0600

    Added key input bound to Spacebar for "flashlight" to follow mouse.

--- src/main/java/me/jurassiklizard/aquatale/Aquatale.java
+++ src/main/java/me/jurassiklizard/aquatale/Aquatale.java
@@ -5,9 +5,12 @@ import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.core.math.Vec2;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.Input;
+import javafx.geometry.Point2D;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -15,6 +18,7 @@ import java.util.HashMap;
 
 public class Aquatale extends GameApplication {
     private Entity player;
+    private Entity light;
     private HashMap<Entity, Node> entityViews = new HashMap<>();
     public static Aquatale instance;
 
@@ -26,19 +30,28 @@ public class Aquatale extends GameApplication {
         gameSettings.setVersion("0.1");
         gameSettings.setIntroEnabled(false);
     }
+    
 
     @Override
     protected void initGame() {
         instance = this;
         Rectangle rectangle = new Rectangle(25, 25, Color.BLUE);
+        Rectangle r = new Rectangle(100, 100, Color.BLACK);
         Vec2 center = Utils.getCenterScreen();
         player = FXGL.entityBuilder()
                 .at(Utils.getRectangleCenterPosition(center.x, center.y, rectangle))
                 .view(rectangle)
                 .buildAndAttach();
+        light = FXGL.entityBuilder()
+                .at(Utils.getRectangleCenterPosition(center.x, center.y, r))
+                .view(r)
+                .buildAndAttach();
+
         FXGL.getGameScene().setCursor(Cursor.DEFAULT);
         FXGL.getGameScene().setBackgroundRepeat("background.png");
         entityViews.put(player, rectangle);
+        entityViews.put(light, r);
+
     }
 
     @Override
@@ -58,11 +71,15 @@ public class Aquatale extends GameApplication {
         FXGL.onKey(KeyCode.S, () -> {
             Utils.move(player, MoveDirection.DOWN); // move down 5 pixels
         });
+
+        FXGL.onKey(KeyCode.SPACE, () -> {
+            Input input = FXGL.getInput();
+            Point2D pos =  input.getMousePositionWorld();
+            light.setPosition(pos); // Update mouse cursor position for flashlight
+        });
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    public static void main(String[] args) { launch(args); }
 
     public Entity getPlayer() {
         return player;
@@ -70,6 +87,15 @@ public class Aquatale extends GameApplication {
 
     public Aquatale setPlayer(Entity player) {
         this.player = player;
+        return this;
+    }
+
+    public Entity getLight() {
+        return light;
+    }
+
+    public Aquatale setLight(Entity light) {
+        this.light = light;
         return this;
     }
 
