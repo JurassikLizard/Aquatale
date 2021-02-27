64ad2f98a8595486d1765de4ac5eeec0fdf3ea92
JurassikLizard <al9999things@gmail.com>
2021-02-26 21:58:35 -0500

    Initial Windoe

--- src/main/java/Aquatale.java
+++ src/main/java/Aquatale.java
@@ -1,2 +1,24 @@
-public class Aquatale {
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
+import javafx.scene.Cursor;
+
+public class Aquatale extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+        gameSettings.setWidth(800);
+        gameSettings.setHeight(600);
+        gameSettings.setTitle("Aquatale");
+        gameSettings.setVersion("0.1");
+        gameSettings.setIntroEnabled(false);
+    }
+
+    @Override
+    protected void initGame() {
+        FXGL.getGameScene().setCursor(Cursor.DEFAULT);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
