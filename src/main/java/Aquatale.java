528c329589758728cb9048f4e0f196cdbe8193b1
JurassikLizard <al9999things@gmail.com>
2021-02-27 09:17:22 -0500

    Player Movement

--- src/main/java/Aquatale.java
+++ src/main/java/Aquatale.java
@@ -4,6 +4,7 @@ import com.almasb.fxgl.core.math.Vec2;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import javafx.scene.Cursor;
+import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import utils.Utils;
@@ -29,6 +30,25 @@ public class Aquatale extends GameApplication {
                 .view(rectangle)
                 .buildAndAttach();
         FXGL.getGameScene().setCursor(Cursor.DEFAULT);
+    }
+
+    @Override
+    protected void initInput() {
+        FXGL.onKey(KeyCode.D, () -> {
+            player.translateX(5); // move right 5 pixels
+        });
+
+        FXGL.onKey(KeyCode.A, () -> {
+            player.translateX(-5); // move left 5 pixels
+        });
+
+        FXGL.onKey(KeyCode.W, () -> {
+            player.translateY(-5); // move up 5 pixels
+        });
+
+        FXGL.onKey(KeyCode.S, () -> {
+            player.translateY(5); // move down 5 pixels
+        });
     }
 
     public static void main(String[] args) {
